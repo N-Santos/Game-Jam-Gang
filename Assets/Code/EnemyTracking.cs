9008c9ff8d034372a824943b91795729f2fb443a
Nick Santos <santos_nick@outlook.com>
2020-11-06 13:35:24 -0800

    Finished the enemy movement
    Movement has adjustable speeds, can edit later if needed

---
+++ Assets/Code/EnemyTracking.cs
@@ -0,0 +1,44 @@
+﻿/*
+    Name: Nicholas Santos
+    Date: 10/20/2020
+    Desc: Movement
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+
+public class EnemyTracking : MonoBehaviour
+{
+    public GameObject Target;
+    public float Speed = 0.5f;
+    private bool LookingRight = true;
+    Rigidbody2D myRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target.transform.position.x > transform.position.x)
+        {
+            LookingRight = true;
+            transform.Translate(2 * Time.deltaTime * Speed, 0, 0);
+            transform.localScale = new Vector2 (2, 2);
+        }
+        else
+        {
+            LookingRight = false;
+            transform.Translate(-2 * Time.deltaTime * Speed, 0, 0);
+            transform.localScale = new Vector2 (2, 2);
+        }
+    }
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿/*
+    Name: Nicholas Santos
+    Date: 10/20/2020
+    Desc: Movement
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 200;
+    public float AngularSpeed = 10;
+    int jump = 0;
+    public UnityEvent OnCollide = new UnityEvent();
+    Rigidbody2D myRb;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        OnCollide.Invoke();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Setup variable to store how we should move
+        Vector2 movement = new Vector2();
+
+        //Check for input and use that to move
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        //Adjust the direction based on input
+        myRb.AddForce(transform.right * movement.x * Speed * Time.deltaTime);
+
+        if(Input.GetKey("space"))
+        {
+            //Adjust the speed based on input
+            myRb.AddForce(transform.up * movement.y);
+        }
+    }
+}
