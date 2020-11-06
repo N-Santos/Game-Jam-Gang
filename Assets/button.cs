4509dd90b9db0b1e8e972d099f55d54705075102
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2020-11-06 14:00:15 -0800

    heres all the code things

---
+++ Assets/button.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Tyler D
+//11/4/2020
+//buttons or somethin
+public class button : MonoBehaviour
+{  
+    //set this as the door you want to be opened
+    public GameObject door;
+    //set this if you want the door to close after x ammount of time
+    public bool isTimed;
+    //this sets the duration the door is open for
+    public float timerDuration = 0;
+    private float timer;
+    private bool buttonActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = timerDuration;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimed == true && buttonActive == true)
+        {
+            if (timer <= 0)
+            {
+                buttonActive = false;
+                door.SetActive(true);
+                timer = timerDuration;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+        }
+        if (buttonActive == true)
+        {
+            door.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)    
+    {        
+        //make sure player is tagged as "Player"
+        if (collision.gameObject.tag == "Player")
+        {           
+            buttonActive = true;
+        }
+    }
+   
+}
